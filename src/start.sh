31f25532e3211ffc23f0328cb633b59b179a09fc
swayam1998 <swayam1998@gmail.com>
2024-08-31 21:37:59 +0530

    moves comfyui installation and environment to network volume

--- src/start.sh
+++ src/start.sh
@@ -1,20 +1,40 @@
 #!/usr/bin/env bash
 
-# Use libtcmalloc for better memory management
-TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
-export LD_PRELOAD="${TCMALLOC}"
+echo "Worker Initiated"
 
-# Serve the API and don't shutdown the container
-if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    echo "runpod-worker-comfy: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
+# Symlink files from Network Volume
+echo "Symlinking files from Network Volume"
+rm -rf /workspace && \
+  ln -s /runpod-volume /workspace
 
-    echo "runpod-worker-comfy: Starting RunPod Handler"
-    python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
+# Activate the virtual environment and start ComfyUI
+if [ -f "/workspace/venv/bin/activate" ]; then
+    echo "Starting ComfyUI in Python Environment"
+    source /workspace/venv/bin/activate
+
+    # Use libtcmalloc for better memory management
+    TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
+    export LD_PRELOAD="${TCMALLOC}"
+    export PYTHONUNBUFFERED=true
+
+    # Serve the API and don't shutdown the container
+    if [ "$SERVE_API_LOCALLY" == "true" ]; then
+        echo "runpod-worker-comfy: Starting ComfyUI"
+        python3 /workspace/ComfyUI/main.py --disable-auto-launch --disable-metadata --listen &
+
+        echo "runpod-worker-comfy: Starting RunPod Handler"
+        python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
+    else
+        echo "runpod-worker-comfy: Starting ComfyUI"
+        python3 /workspace/ComfyUI/main.py --disable-auto-launch --disable-metadata &
+
+        echo "runpod-worker-comfy: Starting RunPod Handler"
+        python3 -u /rp_handler.py
+    fi
+
+    deactivate
 else
-    echo "runpod-worker-comfy: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
-
-    echo "runpod-worker-comfy: Starting RunPod Handler"
-    python3 -u /rp_handler.py
+    echo "ERROR: The Python Virtual Environment (/workspace/venv/bin/activate) could not be activated"
+    echo "1. Ensure that you have followed the installation instructions."
+    echo "2. Ensure that you are using the correct environment and setup."
 fi
